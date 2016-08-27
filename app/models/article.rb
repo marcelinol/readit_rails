7ea4b13590b1e4372e8a30cd8a98bac5bdf20cc4
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-08-27 16:25:05 -0300

    Add validation to article's address and start to test it

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 # :nodoc:
 class Article < Recommendation
+  validates :address, format: {
+    with: URI::regexp,
+    message: 'The address is invalid'
+  }
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,18 @@
 require 'rails_helper'
 
 describe Article do
+  describe 'validations' do
+    context 'when the url is invalid' do
+      it { expect(Article.new(address: 'www.pudim.com.br', title: 'title')).not_to be_valid }
+      it { expect(Article.new(address: 'pudim.com.br', title: 'title')).not_to be_valid }
+      it { expect(Article.new(address: 'xunda', title: 'title')).not_to be_valid }
+
+      it 'has a good message' do
+        article = Article.new(address: 'xunda', title: 'title')
+        article.valid?
+
+        expect(article.errors.messages[:address]).to include('The address is invalid')
+      end
+    end
+  end
 end
