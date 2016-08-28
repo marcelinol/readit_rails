ea1421a2d8aee67b69cab91a361fd6b2378f0c39
pedrohenriquerls <pedro.rodrigues@resultadosdigitais.com.br>
2016-08-28 08:43:13 -0300

    refactor to remove many unnecessary instances

--- app/models/concerns/meta_tags_parser.rb
+++ app/models/concerns/meta_tags_parser.rb
@@ -3,22 +3,22 @@ require 'mechanize'
 #Read recommendation info from metatags
 module MetaTagsParser
   def parse
-    @page = agent_get!(address)
-    description = read_content_from_page('description')
-    title ||= read_content_from_page('title')
-    image = read_content_from_page('image')
+    description, title, image = read_content_from_page('description', 'title', 'image')
   rescue ArgumentError => exception
     puts exception.freeze #log this?
   end
 
   private
-    def agent_get!(url)
+    def agent_get!
       agent = Mechanize.new
-      agent.get(url)
+      agent.get(address)
     end
 
-    def read_content_from_page(property)
-      result = @page.at("meta[property=\"og:#{property}\"]") || {}
-      result[:content]
+    def read_content_from_page(*properties)
+      page = agent_get!
+      properties.collect do |property|
+        result = agent_get!.at("meta[property=\"og:#{property}\"]") || {}
+        send(property.to_sym) || result[:content]
+      end
     end
 end
