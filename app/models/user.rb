76f12944ece03b81ebcf3041b306aa985f7448cb
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-10-02 18:44:37 -0300

    more validations and some refactor on specs to use aggregate_failures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :email
-  validates_uniqueness_of :email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,
+    presence: true,
+    length:   { maximum: 50 }
+
+  validates :email,
+    presence:   true,
+    length:     { maximum: 255 },
+    uniqueness: { case_sensitive: false },
+    format:     { with: VALID_EMAIL_REGEX }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,25 +9,98 @@ RSpec.describe User do
     end
 
     context 'invalid' do
-      context 'without a name' do
-        let(:user) { build(:user, name: '') }
+      context 'when validating name' do
+        let(:user) { build(:user, name: name) }
 
-        it { expect(user).not_to be_valid }
+        context 'without a name' do
+          let(:name) { '' }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).not_to be_valid
+              expect(user.errors.keys).to include(:name)
+              expect(user.errors.full_messages.first).to include("can't be blank")
+            end
+          end
+        end
+
+        context 'with a giant name' do
+          let(:name) { 'x'*51 }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).not_to be_valid
+              expect(user.errors.keys).to include(:name)
+              expect(user.errors.full_messages.first).to include('too long')
+            end
+          end
+        end
       end
 
-      context 'without a email' do
-        let(:user) { build(:user, email: '') }
+      context 'when validating email' do
+        let(:email) { 'dev@example.org' }
+        let(:user)  { build(:user, email: email) }
 
-        it { expect(user).not_to be_valid }
+        context 'without a email' do
+          let(:email) { '' }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).not_to be_valid
+              expect(user.errors.keys).to include(:email)
+              expect(user.errors.full_messages.first).to include("can't be blank")
+            end
+          end
+        end
+
+        context 'with a repeated email' do
+          let(:second_user) { build(:user, name: 'another name', email: email.upcase) }
+          before { user.save! }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).to be_valid
+              expect(second_user).not_to be_valid
+              expect(second_user.errors.keys).to include(:email)
+              expect(second_user.errors.full_messages.first).to include('has already been taken')
+            end
+          end
+        end
+
+        context 'with a giant email' do
+          let(:email) { 'x'*244 + '@example.org' }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).not_to be_valid
+              expect(user.errors.keys).to include(:email)
+              expect(user.errors.full_messages.first).to include('too long')
+            end
+          end
+        end
+
+        context 'with a invalid email address' do
+          let(:email) { 'xunda' }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).not_to be_valid
+              expect(user.errors.keys).to include(:email)
+              expect(user.errors.full_messages.first).to include('is invalid')
+            end
+          end
+
+          let(:email) { 'xunda@example' }
+
+          it 'should not be valid' do
+            aggregate_failures do
+              expect(user).not_to be_valid
+              expect(user.errors.keys).to include(:email)
+              expect(user.errors.full_messages.first).to include('is invalid')
+            end
+          end
+        end
       end
-
-      context 'with a repeated email' do
-        let(:second_user) { build(:user, name: 'another name') }
-        before { user.save! }
-
-        it { expect(second_user).not_to be_valid }
-      end
-
     end
   end
 end
