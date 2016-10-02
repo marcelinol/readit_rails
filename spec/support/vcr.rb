5e0abf315a9c92ce64ed3854c00a04da21e0c6fb
Luciano Medeiros <marcelinolucianom@gmail.com>
2016-10-02 19:08:35 -0300

    add codeclimate coverage report to ignore hosts on vcr

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,4 +1,5 @@
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/vcr_cassettes"
+  config.cassette_library_dir = 'spec/vcr_cassettes'
   config.hook_into :webmock
+  config.ignore_hosts 'codeclimate.com'
 end
