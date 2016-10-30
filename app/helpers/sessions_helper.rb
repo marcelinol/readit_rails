a37ffdb01bc3b4e2734ef86b2876b831b0f37976
marcelinol <marcelinolucianom@gmail.com>
2016-10-30 20:40:01 -0200

    some docs

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,10 +10,12 @@ module SessionsHelper
     @current_user ||= User.find_by_id(session[:user_id])
   end
 
+  # Tells if any user is logged in
   def logged_in?
     !!current_user
   end
 
+  # Log out the current user
   def log_out
     session.delete(:user_id)
     @current_user = nil
